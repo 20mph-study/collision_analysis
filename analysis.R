2b53c59381c1d560acfd0c81fcf6fab380f0fdd6
usr110 <m.aliabbas@gmail.com>
2019-11-06 15:12:56 +0000

    Read and visualize collisions data for 2015

--- analysis.R
+++ analysis.R
@@ -1,6 +1,7 @@
 # Load libraries
 library(rgdal)
 library(leaflet)
+library(tidyverse)
 
 # Load data from the network drive for Belfast road network
 belfast_road_data <- readOGR('V:/Studies/MOVED/HealthImpact/data/20mph study collisions/20mph Speed Limit Streets/20mph_Speed_Limit_Streets.shp')
@@ -31,3 +32,13 @@ edin_cons_streets <- spTransform(edin_cons_streets, "+init=epsg:4326")
 
 # Visualize using leaflet
 leaflet(edin_cons_streets) %>% addTiles() %>% addPolygons()
+
+### Visualize collisions data - 2015 data
+rd <- read_csv("V:\\Studies\\MOVED\\HealthImpact\\Data\\20mph study collisions\\collisions\\collisions 2015.csv")
+
+m <- leaflet(rd) %>% addTiles('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', 
+                              attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>') 
+# Setview to city of Edinburgh
+m %>% setView(55.9492865, 3.1868674, zoom = 8)
+# Add circles
+m %>% addCircles(~Longitude, ~Latitude, weight = 3, radius=40, color="#ffa500", stroke = TRUE, fillOpacity = 0.8) 
