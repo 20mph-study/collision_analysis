88433835cb19b93e6ea571ef06c1a9e9492dc4b4
kellykok <kk704@medschl.cam.ac.uk>
2019-12-06 12:23:49 +0000

    Fixing variable names

--- nearest_line.R
+++ nearest_line.R
@@ -10,15 +10,22 @@ library(mapview)
 library(maptools)
 library(sp)
 library(readxl)
+library(readr)
 
 #1.Function for keeping data of Edinburgh with speed limits 20/30/40
 filter_data <- function(data){
   data <- data %>% filter(data$Speed_limit %in% c(20,30,40)) 
   data <- data %>% filter(data$`Local_Authority_(District)` == 923)
-  data <- as.Date(data$Date,format="%d/%m/%Y")
+  #data$Date <- as.Date(data$Date,format="%d/%m/%Y")
   #data <- na.omit(data[1:31])#remove NA obs
   return(data)
 }
+
+delete_na <- function(data, desiredCols) {
+  completeVec <- complete.cases(data[, desiredCols])
+  return(data[completeVec, ])
+}
+
 
 #Data inputs
 ##1.Data path 
@@ -27,9 +34,11 @@ dir_path <- "C:\\Users\\Kyriaki Kokka\\Desktop\\"
 
 #2.Gis 
 #Read geodatabase for Edinburgh
+#Path
 gdb_path <- paste0(dir_path, "20mph study collisions\\20mph.gdb")
 gdb_layers <- ogrListLayers(gdb_path)
 
+#Shapefiles
 edin_impl_zones <- readOGR(dsn = gdb_path, layer="ImplementationZones")
 edin_cons_streets <- readOGR(dsn = gdb_path,layer="Consultation20mphStreets")
 
@@ -41,9 +50,7 @@ edin_cons_streets <- spTransform(edin_cons_streets, "+init=epsg:4326")
 edin_impl_zones <- spTransform(edin_impl_zones, "+init=epsg:4326")
 
 #3.Read csv files
-
 #read all at once
-library(readr)
 files <- list.files(path = "C:\\Users\\Kyriaki Kokka\\Desktop\\20mph study collisions\\collisions\\", pattern = "*.csv", full.names = T)
 source_data <- sapply(files, read_csv, simplify=FALSE) %>% 
   bind_rows(.id = "id")
@@ -63,9 +70,11 @@ rd_2014 <- rbind(rd_2014,rd_2014b)
 
 #2015 data
 rd_2015 <- read_csv(paste0(dir_path, "20mph study collisions\\collisions\\collisions 2015.csv"))
+rd_2015$Date <- as.Date(rd_2015$Date,format="%d/%m/%Y")
 
 #2018 data
 rd_2018 <- read_csv(paste0(dir_path, "20mph study collisions\\collisions\\collisions 2018.csv"))
+rd_2018$Date <- as.Date(rd_2018$Date,format="%d/%m/%Y")
 
 #2019 data
 #rd_2019 <- read_excel("C:\\Users\\Kyriaki Kokka\\Desktop\\20mph study collisions\\collisions\\collisions 2019 Jan to May Edinburgh only.xls")
@@ -73,6 +82,7 @@ rd_2018 <- read_csv(paste0(dir_path, "20mph study collisions\\collisions\\collis
 
 edin_road_data <-rbind(rd_2013,rd_2014,rd_2015,rd_2018) 
 edin_road_data <- filter_data(edin_road_data)
+edin_road_data <-delete_na(edin_road_data,c("Longitude","Latitude"))
 
 write.csv(edin_road_data, file = "Edin_Data.csv",row.names=FALSE)
 
@@ -102,12 +112,6 @@ nearest_line <-function(df,road_net){
   return(nearest_line_sp)
 }
 
-#Get nearest line per year
-nearest_2013 <- nearest_line(rd_2013,edin_cons_streets)
-nearest_2014 <- nearest_line(rd_2014,edin_cons_streets)
-nearest_2015 <- nearest_line(rd_2015,edin_cons_streets)
-nearest_2018 <- nearest_line(rd_2018,edin_cons_streets)
-
 #Get nearest line for pre and post 20mph 
 nearest_pre <- nearest_line(pre_20,edin_cons_streets)
 nearest_post <- nearest_line(post_20,edin_cons_streets)
@@ -115,21 +119,22 @@ nearest_post <- nearest_line(post_20,edin_cons_streets)
 #Keep nearest line in df
 nearest_pre_line <- data.frame(nearest_pre)
 nearest_post_line <- data.frame(nearest_post)
-nearest_line_2013 <- data.frame(nearest_2013)
 
 #Merge the dataframes based on the ID of the nearest line in order to connect geoinformation with stasts19 
 merged_data <- merge(nearest_pre_line,edin_cons_streets@data,by.x = "nearest_line_id" ,by.y = "ID")
 
 #Keep the columns we need to extract information ( Accident_index, ,Layer, Speed_limit )
-result_pre <- data.frame(merged_data[1],merged_data[2],merged_data[19],merged_data[38])
+result_pre <- data.frame(merged_data[1],merged_data[2],merged_data[19],merged_data[39])
+glimpse(merged_data)
 
 #Count how many accidents happen per layer and per speed limit
 count1 <- aggregate(Accident_Index~LAYER,result_pre,length)
 count2 <- aggregate(Accident_Index~Speed_limit,result_pre,length) 
 
+
+
 #Convert data projections back to lat/long to plot with leaflet
 edin_cons_streets <- spTransform(edin_cons_streets,"+init=epsg:4326")
-rd_final <- spTransform(rd_final, "+init=epsg:4326")
 nearest_pre <- spTransform(nearest_pre, "+init=epsg:4326")
 
 #Leaflet maps of the road network , accidents and their projections
@@ -145,13 +150,13 @@ road_trunk <- filter(edin_cons_streets, edin_cons_streets$LAYER == "Trunk roads"
 #Visualize
 map <- leaflet() %>% addTiles() %>% addPolygons(data = edin_cons_streets)
 
-map_near <-map %>% leaflet() %>% addTiles() %>% addMarkers(lng =total1$X, lat = total1$Y,
-                                                    popup =paste("Accident index: ", total1$Accident_Index, "<br>",
-                                                                 "Speed limit: ", total1$Speed_limit ,"<br>",
-                                                                 "Layer: ",total1$LAYER,"<br>",
-                                                                 "1st road class: ",total1$X1st_Road_Class, "<br>",
-                                                                 "2nd road class: ", total1$X2nd_Road_Class,"<br>",
-                                                                 "Road Type: ",  total1$Road_Type))
+map_near <-map %>% leaflet() %>% addTiles() %>% addMarkers(lng =merged_data$X, lat = merged_data$Y,
+                                                    popup =paste("Accident index: ", merged_data$Accident_Index, "<br>",
+                                                                 "Speed limit: ", merged_data$Speed_limit ,"<br>",
+                                                                 "Layer: ",merged_data$LAYER,"<br>",
+                                                                 "1st road class: ",merged_data$X1st_Road_Class, "<br>",
+                                                                 "2nd road class: ", merged_data$X2nd_Road_Class,"<br>",
+                                                                 "Road Type: ",  merged_data$Road_Type))
 
 map3 <- map_near %>% addCircleMarkers(lng =rd_final@data$Longitude , lat = rd_final@data$Latitude,
                                       popup =paste("Accident index: ", rd_final@data$Accident_Index, "<br>",
