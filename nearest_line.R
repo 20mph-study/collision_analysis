930e9adfc48b29f815d8604eee6cfdf6b66f554d
kellykok <kk704@medschl.cam.ac.uk>
2019-12-06 15:59:00 +0000

    bugs

--- nearest_line.R
+++ nearest_line.R
@@ -17,7 +17,6 @@ filter_data <- function(data){
   data <- data %>% filter(data$Speed_limit %in% c(20,30,40)) 
   data <- data %>% filter(data$`Local_Authority_(District)` == 923)
   #data$Date <- as.Date(data$Date,format="%d/%m/%Y")
-  #data <- na.omit(data[1:31])#remove NA obs
   return(data)
 }
 
