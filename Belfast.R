88b3a1460a54d585e3af050366097c7340551459
usr110 <m.aliabbas@gmail.com>
2019-12-12 11:38:21 +0000

    Use backslash instead for path

--- Belfast.R
+++ Belfast.R
@@ -79,7 +79,7 @@ convert_latlong <-function(rd){
 
 #Road data 
 # Load data from the network drive for Belfast road network
-belfast_road_data <- readOGR('V:/Studies/MOVED/HealthImpact/data/20mph study collisions/20mph Speed Limit Streets/20mph_Speed_Limit_Streets.shp')
+belfast_road_data <- readOGR('V:Studies\\MOVED\\HealthImpact\\data\\20mph study collisions\\20mph Speed Limit Streets\\20mph_Speed_Limit_Streets.shp')
 # Tranform the data by applying a projection
 belfast_road_data <- spTransform(belfast_road_data, "+init=epsg:4326")
 # Visualize using leaflet
