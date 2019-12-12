1b8f766204333f75c01a17950be3fb5485f7d105
usr110 <m.aliabbas@gmail.com>
2019-12-12 12:11:34 +0000

    Use OA instead of SOA

--- Belfast.R
+++ Belfast.R
@@ -89,15 +89,15 @@ belfast_road_data <- spTransform(belfast_road_data, "+init=epsg:4326")
 leaflet(belfast_road_data) %>% addTiles() %>% addPolygons() 
 
 #Output Areas 
-gdb_path_out <- paste0(dir_path, "Belfast Output areas\\SOA2011.shp")
+gdb_path_out <- paste0(dir_path, "OA_ni-ESRI_format\\OA_ni.shp")
 gdb_layers <- ogrListLayers(gdb_path_out)
 print(gdb_layers)
 
-belf_impl_zones_out <- readOGR(dsn = gdb_path_out, layer="SOA2011")
+belf_impl_zones_out <- readOGR(dsn = gdb_path_out, layer="OA_ni")
 #leaflet(belf_impl_zones) %>% addTiles() %>% addPolygons()
 belf_impl_zones_out <- spTransform(belf_impl_zones_out, "+init=epsg:4326")
 inter <- intersect(belfast_road_data, belf_impl_zones_out)
-control_zones_out <- unique(inter@data$SOA_CODE)
+control_zones_out <- unique(inter@data$OA_CODE)
 print(control_zones_out)
 leaflet(inter) %>% addTiles()
 
