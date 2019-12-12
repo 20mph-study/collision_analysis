cbc83b6c46f3b9620dcbdd64a323322913a682fd
usr110 <m.aliabbas@gmail.com>
2019-12-12 11:43:59 +0000

    Define dir_path

--- Belfast.R
+++ Belfast.R
@@ -75,11 +75,14 @@ convert_latlong <-function(rd){
 #-----------------------------------------------------------------------------------------------------------------------------------------------
 
 #2.Gis 
+
+# Define network dir path
+dir_path <- 'V:Studies\\MOVED\\HealthImpact\\data\\20mph study collisions\\'
 #Read geodatabase for Belfast
 
 #Road data 
 # Load data from the network drive for Belfast road network
-belfast_road_data <- readOGR('V:Studies\\MOVED\\HealthImpact\\data\\20mph study collisions\\20mph Speed Limit Streets\\20mph_Speed_Limit_Streets.shp')
+belfast_road_data <- readOGR(paste0(dir_path, '20mph Speed Limit Streets\\20mph_Speed_Limit_Streets.shp'))
 # Tranform the data by applying a projection
 belfast_road_data <- spTransform(belfast_road_data, "+init=epsg:4326")
 # Visualize using leaflet
