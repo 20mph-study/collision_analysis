45d7205d8b82c2fedfe33c8df00e762621dc6789
kellykok <kk704@medschl.cam.ac.uk>
2020-01-17 16:40:07 +0000

    Update maps

--- functions/leaflet_map.R
+++ functions/leaflet_map.R
@@ -28,7 +28,7 @@ leaflet_map <- function(final_df,road_df,edin_streets){
   
   
   #Visualize
-  map <- leaflet() %>% addTiles() %>% addPolygons(data = edin_streets)
+  map <- leaflet() %>% addTiles() %>% addPolylines(data = edin_streets)
   #map <- map %>%  addLegend("bottomright",colors =c("green",  "red", "yellow", "cyan", "blue","black","purple","orange"),
   #labels= c("existing 20mph", "local 20mph","main 20 mph","30mph","40mph", "part time 20mph","50,60 or 70mph","Trunk roads"),
   #title= "Road network",opacity = 1)
@@ -36,26 +36,27 @@ leaflet_map <- function(final_df,road_df,edin_streets){
                                                              popup =paste("Accident index: ", final_df$Accident_Index, "<br>",
                                                                           "Speed limit: ", final_df$Speed_limit ,"<br>",
                                                                           "Layer: ",final_df$LAYER,"<br>",
-                                                                          "Casualty severity: ",final_df$Casualty_Severity))
+                                                                          "Casualty severity: ",final_df$Casualty_Severity), group = 'incidents linked to a road')
   road_df <- (data.frame(road_df))
   map3 <- map_near %>% addCircleMarkers(lng =road_df$Longitude , lat = road_df$Latitude,
                                         popup =paste("Accident index: ", road_df$Accident_Index, "<br>",
                                                      "Speed limit: ", road_df$Speed_limit ,"<br>",
-                                                     "Casualty severity: ",road_df$Casualty_Severity ))
+                                                     "Casualty severity: ",road_df$Casualty_Severity ), group = 'all incidents')
   
-
+  
   map3 <- map3 %>% addLegend("bottomright",colors =c("green",  "red", "yellow", "cyan", "blue","black","purple","orange"),
-                      labels= c("existing 20mph", "local 20mph","main 20 mph","30mph","40mph", "part time 20mph","50,60 or 70mph","Trunk roads"),
-                      title= "Road network",opacity = 1)
+                             labels= c("existing 20mph", "local 20mph","main 20 mph","30mph","40mph", "part time 20mph","50,60 or 70mph","Trunk roads"),
+                             title= "Road network",opacity = 1)
   
   map3 %>%
-  setView(lng = -3.188267 , lat = 55.953251, zoom = 11)%>%
-    addPolylines(data = road_existing_20, color= "green")%>%
-    addPolylines(data = road_local_20,color= "red")%>%
-    addPolylines(data = road_main_20,color= "yellow")%>%
-    addPolylines(data = road_30,color= "cyan")%>%
-    addPolylines(data = road_40,color= "blue")%>%
-    addPolylines(data = road_part,color= "black")%>%
-    addPolylines(data = road_50,color= "purple")%>%
-    addPolylines(data = road_trunk,color= "orange")
+    setView(lng = -3.188267 , lat = 55.953251, zoom = 11)%>%
+    addPolylines(data = road_existing_20, color= "green", group = 'existing 20mph' ) %>%
+    addPolylines(data = road_local_20,color= "red" , group = 'local 20mph' ) %>%
+    addPolylines(data = road_main_20,color= "yellow" , group = 'main 20mph')%>%
+    addPolylines(data = road_30,color= "cyan" , group = '30mph')%>%
+    addPolylines(data = road_40,color= "blue" , group = '40mph')%>%
+    addPolylines(data = road_part,color= "black" , group = 'part time 20mph')%>%
+    addPolylines(data = road_50,color= "purple" , group = '50,60 or 70mph')%>%
+    addPolylines(data = road_trunk,color= "orange", group = 'Trunk roads') %>% addLayersControl(overlayGroups = c( 'existing 20mph','local 20mph','main 20mph','30mph','40mph','part time 20mph','50,60 or 70mph','Trunk roads','incidents linked to a road','all incidents'),
+                                                                                                options = layersControlOptions(collapsed = FALSE))
 }
