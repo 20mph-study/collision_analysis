7efd02df2dc37a5e96307ca13467f456f0b5f213
kellykok <kk704@medschl.cam.ac.uk>
2020-02-11 17:16:31 +0000

    change functions

--- functions/leaflet_map.R
+++ functions/leaflet_map.R
@@ -1,12 +1,11 @@
 #Function for leaflet visualisations
-leaflet_map <- function(final_df,road_df,edin_streets){
+leaflet_map <- function(final_df,edin_streets){
   crs <- CRS( "+proj=utm +zone=32 +ellps=WGS72 +units=m +no_defs")     # UTM zone = 32 N
   wgs84 <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")  # long/lat
   
   #Convert data to planar projection  
   edin_streets <- spTransform(edin_streets,crs)
   coordinates(road_df) <- ~ Longitude + Latitude
-  coordinates(final_df) <- ~ X + Y
   
   #Convert data projections back to lat/long to plot with leaflet
   edin_streets <- spTransform(edin_streets,"+init=epsg:4326")
@@ -29,22 +28,19 @@ leaflet_map <- function(final_df,road_df,edin_streets){
   
   #Visualize
   map <- leaflet() %>% addTiles() %>% addPolylines(data = edin_streets)
-  #map <- map %>%  addLegend("bottomright",colors =c("green",  "red", "yellow", "#41b6c4", "#081d58","#252525","#6a51a3","#ec7014"),
-  #labels= c("existing 20mph", "local 20mph","main 20 mph","30mph","40mph", "part time 20mph","50,60 or 70mph","Trunk roads"),
-  #title= "Road network",opacity = 1)
   map_near <-map %>% leaflet() %>% addTiles() %>% addMarkers(lng =final_df$X, lat = final_df$Y,
                                                              popup =paste("Accident index: ", final_df$Accident_Index, "<br>",
                                                                           "Speed limit: ", final_df$Speed_limit ,"<br>",
                                                                           "Layer: ",final_df$LAYER,"<br>",
                                                                           "Casualty severity: ",final_df$Casualty_Severity), group = 'incidents linked to a road')
   road_df <- (data.frame(road_df))
-  map3 <- map_near %>% addCircleMarkers(lng =road_df$Longitude , lat = road_df$Latitude,
-                                        popup =paste("Accident index: ", road_df$Accident_Index, "<br>",
-                                                     "Speed limit: ", road_df$Speed_limit ,"<br>",
-                                                     "Casualty severity: ",road_df$Casualty_Severity ), group = 'all incidents')
+  map3 <- map_near %>% addCircleMarkers(lng =final_df$Longitude , lat = final_df$Latitude,
+                                        popup =paste("Accident index: ", final_df$Accident_Index, "<br>",
+                                                     "Speed limit: ", final_df$Speed_limit ,"<br>",
+                                                     "Casualty severity: ",final_df$Casualty_Severity ), group = 'all incidents')
   
   
-  map3 <- map3 %>% addLegend("bottomright",colors =c("#41ab5d",  "#fc4e2a", "#ffeda0",  "#1d91c0", "#081d58","#252525","#6a51a3","#ec7014"),
+  map3 <- map3 %>% addLegend("bottomright",colors =c("#41ab5d",  "#fc4e2a", "#ffeda0",  "#1d91c0", "#081d58","#e7298a","#6a51a3","#ec7014"),
                              labels= c("existing 20mph", "local 20mph","main 20 mph","30mph","40mph", "part time 20mph","50,60 or 70mph","Trunk roads"),
                              title= "Road network",opacity = 1)
   
@@ -55,7 +51,7 @@ leaflet_map <- function(final_df,road_df,edin_streets){
     addPolylines(data = road_main_20,color= "#ffeda0" , group = 'main 20mph')%>%
     addPolylines(data = road_30,color= "#1d91c0" , group = '30mph')%>%
     addPolylines(data = road_40,color= "#081d58" , group = '40mph')%>%
-    addPolylines(data = road_part,color= "#252525" , group = 'part time 20mph')%>%
+    addPolylines(data = road_part,color= "#e7298a" , group = 'part time 20mph')%>%
     addPolylines(data = road_50,color= "#6a51a3" , group = '50,60 or 70mph')%>%
     addPolylines(data = road_trunk,color= "#ec7014", group = 'Trunk roads') %>% addLayersControl(overlayGroups = c( 'existing 20mph','local 20mph','main 20mph','30mph','40mph','part time 20mph','50,60 or 70mph','Trunk roads','incidents linked to a road','all incidents'),
                                                                                                 options = layersControlOptions(collapsed = FALSE))
