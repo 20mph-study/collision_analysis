c685a7a05f4d4556d34fffa4f388766887dff134
kellykok <kk704@medschl.cam.ac.uk>
2020-02-17 15:54:55 +0000

    fix code for zones

--- functions/nearest_line.R
+++ functions/nearest_line.R
@@ -6,7 +6,7 @@ nearest_line <-function(df,road_net){
   
   #Convert data to planar projection  
   road_net <- spTransform(road_net,crs)
-  df <-  spTransform(SpatialPointsDataFrame(coords = df[34:35],proj4string = wgs84,data = df), crs)
+  df <-  spTransform(SpatialPointsDataFrame(coords = df[35:36],proj4string = wgs84,data = df), crs)
   
   #Make sure our data have same projections
   proj4string(df) <- proj4string(road_net)
