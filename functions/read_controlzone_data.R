a0b675343b3dbeb61c322377dc4e1c545e05ad5d
kellykok <kk704@medschl.cam.ac.uk>
2020-02-17 18:19:49 +0000

    add function

---
+++ functions/read_controlzone_data.R
@@ -0,0 +1,6 @@
+read_controlzone_data <- function(){
+  control_zone_data <- read_csv(paste0(dir_path, "/edin_control_data.csv"))
+  control_zone_data <- control_zone_data %>% filter(control_zone_data$Speed_limit %in% c(20,30)) 
+  control_zone_data <- control_zone_data %>% mutate(year = format(as.Date(control_zone_data$Date, format="%m/%d/%Y"),"%Y"))
+  return(control_zone_data)
+}
