0c14361253f5021cd8daeee7e99d9933c8334a1d
kellykok <kk704@medschl.cam.ac.uk>
2020-02-25 16:13:37 +0000

    Change dates,fix tables

--- functions/belf_annual_table.R
+++ functions/belf_annual_table.R
@@ -1,38 +1,33 @@
-belf_annual_table <- function(belf_data,pre,post){
+belf_annual_table <- function(total_zones,total,pre,post){
   source('../functions/belcount_pre.R')
   
-  sum_pre <- pre[pre$a_speed == 20,2] + pre[pre$a_speed == 30,2] 
-  sum_post <- post[post$a_speed == 20,2] + post[post$a_speed == 30,2] 
+  not_changed <- delete_na(total_zones[!(total_zones$a_ID %in% total$a_ID),],"a_ID")
   
-  sum1 <- c(nrow(belf_data[belf_data$a_date >= "2013-02-01" & belf_data$a_date <= "2016-01-31",]))
-  sum2 <- c(nrow(belf_data[belf_data$a_date >= "2016-02-02" & belf_data$a_date <= "2019-03-31",]))
+  sum1 <- c(nrow(total_zones[total_zones$a_date >= "2013-02-01" & total_zones$a_date <= "2016-01-31",]))
+  sum2 <- c(nrow(total_zones[total_zones$a_date >= "2016-02-02" & total_zones$a_date <= "2019-03-31",]))
   
   #belcount_pre
-  not_changed <- belf_data
-  pre_not_changed <- belcount_pre(not_changed[not_changed$a_date >= "2013-02-01" & not_changed$a_date <= "2016-01-31",] )
-  post_not_changed <- belcount_pre(not_changed[not_changed$a_date >= "2016-02-02" & not_changed$a_date <= "2019-03-31",] )
+  not_changed_pre <-  c(nrow(not_changed[not_changed$a_date >= "2013-02-01" & not_changed$a_date <= "2016-01-31",]))
+  not_changed_post <- c(nrow(not_changed[not_changed$a_date >= "2016-02-02" & not_changed$a_date <= "2019-03-31",]))
   
-  pre_not_changed_20 <- pre_not_changed[1,2] 
-  pre_not_changed_30 <- pre_not_changed[2,2] 
-  
-  post_not_changed_20 <-  post_not_changed[1,2]
-  post_not_changed_30 <-  post_not_changed[2,2]
-  
-  collisions_pre_20mph <- c(sum_pre, pre_not_changed_20 ,pre_not_changed_30 , sum1)
-  collisions_post_20mph <- c(sum_post, post_not_changed_20,post_not_changed_30, sum2)
+  changed_pre <- sum1 - not_changed_pre
+  changed_post <- sum2 - not_changed_post
+
+  collisions_pre_20mph <- c(changed_pre, not_changed_pre, sum1)
+  collisions_post_20mph <- c(changed_post, not_changed_post, sum2)
   
   rate_pre_20mph <- round(collisions_pre_20mph / 3, 2)
-  rate_post_20mph <- round(collisions_post_20mph / 2.16, 2)
+  rate_post_20mph <- round(collisions_post_20mph / 3.16, 2)
   
   diff_in_rates <-  rate_pre_20mph - rate_post_20mph
   percentage_diff_in_rates <- round(diff_in_rates / rate_pre_20mph * 100, 2)
   
-  type <- c("Streets changed to 20mph","at 20mph ","at 30mph","All streets")
+  type <- c("Streets changed to 20mph","at 30mph","All streets")
   df <- data.frame(type, collisions_pre_20mph, collisions_post_20mph, rate_pre_20mph, rate_post_20mph, diff_in_rates, percentage_diff_in_rates)
   colnames(df)=c("Type", "collisions pre-20mph (2013 / 2016 - 3 years)", "collisions post-20mph (2016 / 2019 - 26 months)", "rate pre-20mph", "rate post-20mph","diff in rates = pre - post", "Perc.diff.rates = (pre - post) / pre * 100")
   kable(df,caption = "Table 1: Average annual road traffic collision rates in the city of Belfast (PSNI data)") %>% kable_styling(bootstrap_options = c("striped","hover")) %>%
     footnote(general = " ", number =c("All accidents at 20mph, 30mph")) %>% 
-    pack_rows("Streets stayed", 2, 3,label_row_css = "background-color: #e5f5e0 ;") %>% 
+    pack_rows("Streets stayed", 2, 2,label_row_css = "background-color: #e5f5e0 ;") %>% 
     pack_rows("Streets changed", 1, 1,label_row_css = "background-color: #e5f5e0;") %>% 
-    pack_rows("All streets", 4, 4,label_row_css = "background-color:#e5f5e0 ;")
+    pack_rows("All streets", 3, 3,label_row_css = "background-color:#e5f5e0 ;")
 }
