607310e1b491031874ae28b7ec5ea80048eec961
kellykok <kk704@medschl.cam.ac.uk>
2020-03-12 14:40:15 +0000

    fix code

--- functions/read_road_data.R
+++ functions/read_road_data.R
@@ -16,7 +16,8 @@ read_road_data <- function(){
   rd_2019a <- read_excel(paste0(dir_path, "/2019 DataJune.xls"))
 
   rd_2019b <- read_excel(paste0(dir_path, "/2019 Data - July to December.xls"))
-  rd_2019 <- rbind(rd_2019a,rd_2019b)
+  
+  rd_2019 <- rbind.fill(rd_2019a,rd_2019b)
   rd_2019 <- rd_2019 %>% filter(rd_2019$`Speed Limit` %in% c(20,30)) 
   coord <- rd_2019 %>% st_as_sf(coords = c("Grid Ref: Easting","Grid Ref: Northing"), crs = 27700) %>% st_transform(4326) %>% st_coordinates() %>% as_tibble()
   rd_2019 <- data.frame(rd_2019,coord)
